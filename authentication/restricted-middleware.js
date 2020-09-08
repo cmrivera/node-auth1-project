f343b1fe3be1a324120f1b756ba13e9c891ef6a2
cmrivera <cmrivera0004@student.stcc.edu>
2020-09-08 18:01:24 -0400

    changes made with support hours. MVP met

--- authentication/restricted-middleware.js
+++ authentication/restricted-middleware.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   //check that we remember the client,
   //that the client logged in already
+  /** 2 - 3 Server verifies credentials then creates session */
   if (req.session && req.session.user) {
     next();
   } else {
